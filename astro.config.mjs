93e5ead47786e5cf6ab8cbf3e1d0e5ff56a14079
Lordor5 <thelordor5@gmail.com>
2023-12-10 19:02:54 +0100

    funcionara?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lordor5.github.io',
 	integrations: [mdx(), sitemap()],
 });
