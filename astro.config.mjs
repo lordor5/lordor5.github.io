4b03b872710e21d34414423dd597fe19c7674181
Lordor5 <thelordor5@gmail.com>
2023-12-23 20:09:24 +0100

    si

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import robots from "astro-robots";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lordor5.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://lordor5.github.io',
+  integrations: [mdx(), sitemap(), robots()]
 });
